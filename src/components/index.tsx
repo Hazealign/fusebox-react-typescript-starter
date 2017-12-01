5e34ca0b2647d989b7ac606ac4810b1821d95573
Haze Lee <hazelee@realignist.me>
2017-12-01 18:38:25 +0900

    Initial Commit

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import Main from './Main'
+import './index.less'
+
+ReactDOM.render(<Main />, document.querySelector('#demo'))
